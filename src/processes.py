728187093acaf67e84dd1cc3e2be13bed2649b4d
karuj <jonathan.karu@gmail.com>
2019-09-13 18:16:01 +0300

    If ball is detected, move closer

--- src/processes.py
+++ src/processes.py
@@ -1,43 +1,28 @@
-from multiprocessing import Process, Value
-
 import imageprocessing
 import time
 
 """
-right_wheel_angle = 120
-middle_wheel_angle = 0
-left_wheel_angle = 240
+    serial_worker - function that supervises the serial process to stop it when necessary
+    serial_process - process that sends the speeds for the motors over serial
+    vision_worker - supervises the vision process to stop it when necessary
+    vision_process - process that detects balls and basket from the image 
+        and modifies the current speeds to be sent
 """
-
 def serial_worker(run, Robo_serial, processes_variables):
-
     while 1:
         if run.value:
             serial_process(Robo_serial, processes_variables)
 
 def serial_process(Robo_serial, speeds):
-    print("serial thread")
-
     Robo_serial.speeds = speeds[:3]
-    
     Robo_serial.send_speeds()
 
-
-
-
-#changed arguments name
 def vision_worker(run, process_variables):
     while 1:
         if run.value:
             vision_process(process_variables)
 
-
-
-"""
-
-"""
 def vision_process(process_variables):
-    print("vision thread")
     imageprocessing.start(process_variables)
 
     
